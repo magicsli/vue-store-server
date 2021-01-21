c46abc25037ddddd44570c0e0ea517392f0cd0a1
magicsli <2544510157@qq.com>
2021-01-21 18:10:17 +0800

    fix: 添加socket成功

--- socketIO/socketIO_server.js
+++ socketIO/socketIO_server.js
@@ -1,24 +1,38 @@
-const {ChatModel} = require('../db/models')
+const { ChatModel } = require("../db/models");
 
 module.exports = function (server) {
-    const io = require('socket.io')(server)
+  const io = require("socket.io")(server, {
+    cors: {
+      origin: "http://localhost:5200",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true,
+    },
+  });
+  setTimeout(() => {
+    io.emit("as", "sss");
+  }, 2000);
+  //  监视客户端和服务器的链接
+  io.on("connection", function (socket) {
+    console.log("");
 
-    //  监视客户端和服务器的链接
-    io.on('connection', function (socket) {
-        console.log('链接成功')
+    // 绑定监听, 监听客户端发过来的消息
+    socket.on("sendMsg", function ({ from, to, content }) {
+      // 保存消息
+      const chat_id = [from, to].sort().join("_"),
+        create_time = Date.now();
+      new ChatModel({
+        from,
+        to,
+        content,
+        chat_id,
+        create_time,
+        read: false,
+      }).save(function (err, chatMsg) {
+        // 保存数据后, 要向对象发送
 
-        // 绑定监听, 监听客户端发过来的消息
-        socket.on('sendMsg', function ({from, to, content}) {
-            
-            // 保存消息
-            const chat_id = [from, to].sort().join('_'),
-            create_time = Date.now();    
-            new ChatModel({from, to, content, chat_id, create_time, read:false}).save(function(err, chatMsg){
-                // 保存数据后, 要向对象发送
-     
-                io.emit('receiveMsg', chatMsg)
-            })
-        })
-  
-    })
-}
+        io.emit("receiveMsg", chatMsg);
+      });
+    });
+  });
+};
