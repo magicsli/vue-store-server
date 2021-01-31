dd595fc9c03085976ff3f22f8190290d1bc750b1
magicsli <2544510157@qq.com>
2021-01-31 15:30:01 +0800

    feat: 聊天功能完成

--- socketIO/socketIO_server.js
+++ socketIO/socketIO_server.js
@@ -1,5 +1,8 @@
 const { ChatModel } = require("../db/models");
 
+// 链接映射表
+const linkMap = {};
+
 module.exports = function (server) {
   const io = require("socket.io")(server, {
     cors: {
@@ -7,16 +10,20 @@ module.exports = function (server) {
       methods: ["GET", "POST"],
       allowedHeaders: ["my-custom-header"],
       credentials: true,
-      
     },
   });
   //  监视客户端和服务器的链接
   io.on("connection", function (socket) {
-    console.log("");
+    // console.log(socket.client);
+
+    // socket.on("sendMsg", function({})
     
+    socket.on("bindUserSocket", function ({_id}) {
+      linkMap[_id] = socket.id
+    });
+
     // 绑定监听, 监听客户端发过来的消息
     socket.on("sendMsg", function ({ from, to, content }) {
-
       // 保存消息
       const chat_id = [from, to].sort().join("_"),
         create_time = Date.now();
@@ -28,9 +35,12 @@ module.exports = function (server) {
         create_time,
         read: false,
       }).save(function (err, chatMsg) {
+
         // 保存数据后, 要向对象发送
-        io.to(socket.id).emit("receiveMsg", err || chatMsg);
+        io.to(linkMap[from]).emit("receiveMsg", err || chatMsg);
+        io.to(linkMap[to]).emit("receiveMsg", err || chatMsg);
       });
     });
+
   });
 };
